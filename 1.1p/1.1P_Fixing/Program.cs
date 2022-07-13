2394eaaae04c839dd2a20441fec1b5065674eb6c
lachlan robinson <lachy4242@hotmail.com>
2022-07-13 16:16:33 +1000

    First commit

---
+++ 1.1p/1.1P_Fixing/Program.cs
@@ -0,0 +1,11 @@
+﻿class DebuggingCode
+{
+    static void Main(string[] args)
+    {
+        int number = 50;
+        if (number == 50)
+        {
+            Console.WriteLine("Number is 50");
+        }
+    }
+}
---
+++ 1.1p/1.1P_SwitchStatement/Program.cs
@@ -0,0 +1,30 @@
+﻿class SwitchStatement
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            Console.WriteLine("Enter a number (as an integer): ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            switch (number)
+            {
+                case 1: Console.WriteLine("ONE"); break;
+                case 2: Console.WriteLine("TWO"); break;
+                case 3: Console.WriteLine("THREE"); break;
+                case 4: Console.WriteLine("FOUR"); break;
+                case 5: Console.WriteLine("FIVE"); break;
+                case 6: Console.WriteLine("SIX"); break;
+                case 7: Console.WriteLine("SEVEN"); break;
+                case 8: Console.WriteLine("EIGHT"); break;
+                case 9: Console.WriteLine("NINE"); break;
+                default: Console.WriteLine("You did not enter a number between 1 and 9."); break;
+            }
+            Console.ReadLine();
+        }
+        catch
+        {
+            Console.WriteLine("ERROR");
+        }
+    }
+}
