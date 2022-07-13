1700d9a6376fbb3d674548ad8bc021c97b24a78d
lachlan robinson <lachy4242@hotmail.com>
2022-07-13 16:52:18 +1000

    1.1P Microwave

---
+++ 1.1p/1.1P_microwave/microwave.cs
@@ -0,0 +1,42 @@
+﻿class Microwave
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            Console.WriteLine("How many items are you microwaving?");
+            int number = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("How long does it take to microwave a single item?");
+            double time = Convert.ToDouble(Console.ReadLine());
+            
+            if (number == 1)
+            {
+                Console.WriteLine("Microwave time: " + time);
+            }
+            else if (number == 2)
+            {
+                Console.WriteLine("Microwave time: " + time * 1.5);
+            }
+            else if (number == 3)
+            {
+                Console.WriteLine("Microwave time: " + time * 2);
+            }
+            else if (number > 3)
+            {
+                Console.WriteLine("We do not recommend you microwave this many items.");
+            }
+            else if (number == 0)
+            {
+                Console.WriteLine("You can't microwave nothing...");
+            }
+            else if (number <= 0)
+            {
+                Console.WriteLine("Error");
+            }
+        }
+        catch
+        {
+            Console.WriteLine("Error");
+        }
+    }
+}
