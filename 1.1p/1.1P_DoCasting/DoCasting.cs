dd32fd13e399b9a96ccc29237c57faa5e08dc224
lachlan robinson <lachy4242@hotmail.com>
2022-07-13 21:35:44 +1000

    Finished Part 6

---
+++ 1.1p/1.1P_DoCasting/DoCasting.cs
@@ -0,0 +1,18 @@
+﻿class DoCasting
+{
+    static void Main(string[] args)
+    {
+        int sum = 17;
+        int count = 5;
+
+        int intAverage;
+        intAverage = sum / count;
+        Console.WriteLine(intAverage);
+
+        double doubleAverage;
+        doubleAverage = sum / count;
+        Console.WriteLine(doubleAverage);
+
+        Console.WriteLine((double)sum / count);
+    }
+}
