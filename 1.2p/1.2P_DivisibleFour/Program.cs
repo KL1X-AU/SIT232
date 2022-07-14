04111cd1d23db1e4f164a2bc0dee87a6d2ecbc49
Lachlan <lachy4242@hotmail.com>
2022-07-14 11:18:48 +1000

    1.2P

---
+++ 1.2p/1.2P_DivisibleFour/Program.cs
@@ -0,0 +1,23 @@
+﻿class DivisibleFour
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            Console.WriteLine("Please input an integer value (n):");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                if (i % 4 == 0 && i % 5 != 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+        catch (System.Exception)
+        {
+            Console.WriteLine("An Error Occurred");
+        }
+    }
+}
