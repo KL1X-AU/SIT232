b65b4e934870fbc7c3a85cd3463fe08be0428240
Lachlan <lachy4242@hotmail.com>
2022-07-14 10:49:31 +1000

    Workshop 1

---
+++ 1.2p/1.2P_GuessingNumber/Program.cs
@@ -0,0 +1,27 @@
+﻿class GuessingNumber
+{
+   static void Main(string[] args){
+        int number = 5;
+        
+        bool loop = true;
+        do
+        {
+            Console.WriteLine("Enter a number between 1 and 10.");
+            int guess = Convert.ToInt32(Console.ReadLine());
+
+            if (guess == number)
+            {
+                Console.WriteLine("You got it!");
+                loop = false;
+            }
+            else if (guess <= number)
+            {
+                Console.WriteLine("Too low.");
+            }
+            else if (guess >= number)
+            {
+                Console.WriteLine("Too high.");
+            }
+        } while (loop == true);
+    }
+}
---
+++ 1.2p/1.2P_Repetition/Program.cs
@@ -0,0 +1,32 @@
+﻿class Repitition
+{
+    static void Main(string[] args)
+    {
+        int sum = 0;
+        double average;
+        int upperbound = 100;
+
+        //for (int number = 1; number <= upperbound; number++)
+        //{
+        //    sum += number;
+        //}
+
+        //int number = 1;
+        //while (number <= upperbound)
+        //{
+        //    sum += number;
+        //    number++;
+        //}
+
+        int number = 1;
+        do
+        {
+            sum += number;
+            number++;
+        } while (number <= 100);
+
+        average = (double)sum / upperbound;
+        Console.WriteLine($"Average: {average}");
+        Console.WriteLine($"Sum: {sum}");
+    }
+}
