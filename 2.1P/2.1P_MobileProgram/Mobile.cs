4b61f636bb6739e99bb994ee029e811cc6b9c492
Lachlan <lachy4242@hotmail.com>
2022-07-14 14:44:41 +1000

    2.1P

---
+++ 2.1P/2.1P_MobileProgram/Mobile.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+class Mobile
+{
+    private String accType, device, number;
+    private double balance;
+    public Mobile(string accType, string device, string number)
+    {
+        this.accType = accType;
+        this.device = device;
+        this.number = number;
+        this.balance = 0.0;
+    }
+
+    //Accessor Methods
+    public String getAccType()
+    {
+        return this.accType;
+    }
+    public String getDevice()
+    {
+        return this.device;
+    }
+    public String getNumber()
+    {
+        return this.number;
+    }
+    public String getBalance()
+    {
+        return this.balance.ToString("C");
+    }
+
+    //Mutator Methods
+    public void setAccType(String accType)
+    {
+        this.accType = accType;
+    }
+    public void setDevice(String device)
+    {
+        this.device = device;
+    }
+    public void setNumber(String number)
+    {
+        this.number = number;
+    }
+    public void setBalance(double balance)
+    {
+        this.balance = balance;
+    }
+}
---
+++ 2.1P/2.1P_MobileProgram/Program.cs
@@ -0,0 +1,27 @@
+﻿class MobileProgram
+{
+    static void Main(string[] args)
+    {
+        Mobile jimMobile = new Mobile("Monthly", "Samsung Glaxy S6", "07712223344");
+
+        MobileProgram pr = new MobileProgram();
+
+        pr.checkAccount(jimMobile);
+
+        jimMobile.setAccType("PAYG");
+        jimMobile.setDevice("Iphone 6S");
+        jimMobile.setNumber("0449005985");
+        jimMobile.setBalance(15.50);
+
+        pr.checkAccount(jimMobile);
+    }
+
+    public void checkAccount(Mobile account)
+    {
+        Console.WriteLine($"Account Type: \t{account.getAccType()}");
+        Console.WriteLine($"Mobile Number: \t{account.getNumber()}");
+        Console.WriteLine($"Device:  \t{account.getDevice()}");
+        Console.WriteLine($"Balance: \t{account.getBalance()}");
+        Console.ReadLine();
+    }
+}
